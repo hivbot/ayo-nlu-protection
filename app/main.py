b568ce7ca61487cd96a55c8fa1d293ba94bf76e9
schwasa <102740850+schwasa@users.noreply.github.com>
2024-07-23 21:09:39 +0200

    Update

--- app/main.py
+++ app/main.py
@@ -7,13 +7,6 @@ from pydantic import BaseModel
 from typing import Optional
 from fastapi.responses import JSONResponse
 
-
-
-AYO_WHATSAPP_API = os.environ.get('AYO_WHATSAPP_API')
-PHONE_NUMBER_ID = os.environ.get('PHONE_NUMBER_ID')
-VF_DM_URL = os.environ.get('VF_DM_URL')
-VF_API_KEY = os.environ.get('VF_API_KEY')
-
 app = FastAPI()
 logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO").upper())
 logger = logging.getLogger(__name__)
@@ -35,12 +28,13 @@ async def protection_variables(variables_input: BaseInput):
     try:
         user_id = variables_input.user_id
         user_name = variables_input.user_name
-        
+
         logger.info("Received post request:")
         logger.info("user_id: %s", user_id)
         logger.info("user_name: %s", user_name)
 
-        response = ayo_nlu_protection.patch_user_variables(VF_DM_URL, user_id, user_name, VF_API_KEY)
+        response = ayo_nlu_protection.patch_user_variables(user_id, user_name)
+        logger.info("response: %s", response)
         return JSONResponse(content={"message": "Patch request successful"})
         return JSONResponse(content={"message": response})
 
@@ -54,17 +48,23 @@ async def protection_variables(variables_input: BaseInput):
 async def protection_interact(interact_input: ProtectionInput):
     try:
         user_id = interact_input.user_id
+        user_name = interact_input.user_name
         session = interact_input.session
-        request = interact_input.request
-        DMconfig = interact_input.DMconfig
+        request = interact_input.action
+        DMconfig = interact_input.config
 
-        logger.info("Received scheduler get request:")
+        logger.info("Received post request:")
         logger.info("user_id: %s", user_id)
         logger.info("session: %s", session)
         logger.info("user_enquiry: %s", request)
         logger.info("DMconfig: %s", DMconfig)
 
         response = ayo_nlu_protection.post_user_enquiry(DMconfig, session, user_id, request)
+        # response = ayo_nlu_protection.analyze_text(request)
+        logger.info("response: %s", response)
+        # response = ayo_nlu_protection.anonymize_text(request,response)
+        # logger.info("response: %s", response)
+
         return JSONResponse(content={"message": "Post request successful"})
         return JSONResponse(content={"message": response})
 
